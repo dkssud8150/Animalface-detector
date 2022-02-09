d10de6c4e4dd31b93e631f4b4a0d6409b011db62
Yoon Jae Ho <dkssud8159@naver.com>
2022-02-09 23:17:26 +0900

    master

--- download_font.py
+++ download_font.py
@@ -2,8 +2,11 @@ import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 
+# print(fm.findSystemFonts(fontpaths=None, fontext='ttf'))
+
 # 한글 폰트 설정하기
-fontpath = '/usr/share/fonts/truetype/nanum/NanumBarunGothic.ttf'
-font = fm.FontProperties(fname=fontpath, size=10)
-plt.rc('font', family='NanumBarunGothic')
-fm._rebuild()
+
+fontpath = 'C:/Windows/Fonts/NanumGothicLight.ttf'
+font = fm.FontProperties(fname=fontpath, size=10).get_name()
+plt.rc('font', family=font)
+
