45c30127840336fff155af154efd834158c36315
Yoon Jae Ho <dkssud8159@naver.com>
2022-02-10 14:16:51 +0900

    master

--- app.py
+++ app.py
@@ -9,6 +9,16 @@ import matplotlib.pyplot as plt
 import torchvision
 from torchvision import datasets, models, transforms
 import os
+import matplotlib
+import matplotlib.font_manager as fm
+
+# print(fm.findSystemFonts(fontpaths=None, fontext='ttf'))
+
+# 한글 폰트 설정하기
+fontpath = 'C:/Windows/Fonts/NanumGothicLight.ttf'
+font = fm.FontProperties(fname=fontpath, size=10).get_name()
+plt.rc('font', family=font)
+
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -82,15 +92,14 @@ def hello():
 def upload_image_file():
     if request.method == 'POST':
         file = request.files['uploaded_image']
-
-        image_bytes = file.read()
-        result = request.form
+        
         if not file: return "No Files"
+        file = file.read()
 
         # 분류 결과 확인 및 클라이언트에게 결과 반환
-        class_name = get_prediction(image_bytes=image_bytes)
-        print("결과:", {'class_name': class_name})
-        return render_template('upload.html', label = class_name, result = result) 
+        class_name = get_prediction(image_bytes=file)
+        # print("결과:", {'class_name': class_name})
+        return jsonify({"class_name": class_name}) 
     else:
         return jsonify({"Methods : ":request.method})
     
@@ -102,10 +111,11 @@ def testing():
         result = request.form
         return render_template('test.html',label = result)
     else:
-        return jsonify({"never die":request.method})
+        return render_template('test.html',label = request.method)
 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
 
 
