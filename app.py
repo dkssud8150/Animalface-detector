2a8091f86acb4d43de6373578cc9479ae8a35284
Yoon Jae Ho <dkssud8159@naver.com>
2022-02-10 13:34:21 +0900

    master

--- app.py
+++ app.py
@@ -4,12 +4,29 @@
 from PIL import Image
 import torch
 from torchvision import transforms
+import numpy as np
+import matplotlib.pyplot as plt
+import torchvision
+from torchvision import datasets, models, transforms
+import os
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+transforms_train = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(), # 데이터 증진(augmentation)
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # 정규화(normalization)
+])
 transforms_test = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
+
+data_dir = './data'
+train_datasets = datasets.ImageFolder(os.path.join(data_dir,'train'), transforms_train)
+class_names = train_datasets.classes
 
 model = torch.load("./weight/model_best_epoch.pt")
 
@@ -57,10 +74,29 @@ run_with_ngrok(app)
 
 @app.route('/')
 def hello():
-    return render_template('index.html', label = "pleases click")
+    return render_template('index.html')
 
 
-@app.route('/test', methods=['GET','POST'])
+
+@app.route('/upload_image', methods=['POST','GET'])
+def upload_image_file():
+    if request.method == 'POST':
+        file = request.files['uploaded_image']
+
+        image_bytes = file.read()
+        result = request.form
+        if not file: return "No Files"
+
+        # 분류 결과 확인 및 클라이언트에게 결과 반환
+        class_name = get_prediction(image_bytes=image_bytes)
+        print("결과:", {'class_name': class_name})
+        return render_template('upload.html', label = class_name, result = result) 
+    else:
+        return jsonify({"Methods : ":request.method})
+    
+
+
+@app.route('/test', methods=['POST','GET'])
 def testing():
     if request.method == 'POST':
         result = request.form
@@ -69,21 +105,6 @@ def testing():
         return jsonify({"never die":request.method})
 
 
-@app.route('/upload_image', methods=['GET','POST'])
-def upload_image_file():
-    if request.method == 'POST':
-        file = request.files['uploaded_image']
-        if not file: return render_template('upload.html', label="No Files")
-
-        image_bytes = file.read()
-
-        # 분류 결과 확인 및 클라이언트에게 결과 반환
-        class_name = get_prediction(image_bytes=image_bytes)
-        print("결과:", {'class_name': class_name})
-        return render_template('upload.html', label=class_name)
-    else:
-        return jsonify({"class_name":request.method})
-    
 if __name__ == "__main__":
     app.run()
 
